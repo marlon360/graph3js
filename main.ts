e863285ec80050110632f8e8ca8b990dd97b508a
Marlon Lückert <m.lueckert@me.com>
2019-11-30 22:17:04 +0100

    create graph

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 import {GraphViewer} from './src/GraphViewer';
 
 const graphViewer = new GraphViewer(document.body);
+graphViewer.createGraph((x,y) => Math.sign(x) * (y / 2));
--- src/GraphViewer.ts
+++ src/GraphViewer.ts
@@ -7,13 +7,14 @@ import {
     MeshBasicMaterial,
     PlaneGeometry,
     Mesh,
+    Material,
     MeshNormalMaterial,
     MeshLambertMaterial,
     DoubleSide,
     TextureLoader,
     RepeatWrapping,
     VertexColors,
-    Vector3
+    Vector3, ParametricGeometry, Color, Face3
 } from 'three';
 
 import OrbitControls from 'three-orbitcontrols';
@@ -100,6 +101,82 @@ export class GraphViewer {
 
     render() {
         this.renderer.render(this.scene, this.camera);
+    }
+
+    public createGraph(func: (x,y) => number, setting?: {
+        xMin: number,
+        xMax: number,
+        yMin: number,
+        yMax: number,
+        segments: number
+    }) {
+
+        // set default values
+        const xMin = setting && setting.xMin || -100,
+              xMax = setting && setting.xMax || 100,
+              yMin = setting && setting.yMin || -100,
+              yMax = setting && setting.yMin || 100,
+              segments = setting && setting.segments || 40;
+        
+        // calculate ranges
+        const xRange = xMax - xMin;
+        const yRange = yMax - yMin;
+        
+        // x and y from 0 to 1
+        const meshFunction = (x, y, vec3) => {
+            // map x,y to range
+            x = xRange * x + xMin;
+            y = yRange * y + yMin;
+            // get z value from function
+            const z = func(x, y);
+            if (!isNaN(z))
+                vec3.set(x, y, z);
+        };
+
+        const graphGeometry = new ParametricGeometry(meshFunction, segments, segments);
+
+        // set colors based on z value
+        graphGeometry.computeBoundingBox();
+        const zMin = graphGeometry.boundingBox.min.z;
+        const zMax = graphGeometry.boundingBox.max.z;
+        const zRange = zMax - zMin;
+               
+        // first, assign colors to vertices
+        for (var i = 0; i < graphGeometry.vertices.length; i++) {
+            const point = graphGeometry.vertices[i];
+            const color = new Color(0xffffff);
+            // only change color if not infinte
+            if (isFinite(zRange)) {
+                color.setHSL(0.7 * (zMax - point.z) / zRange, 1, 0.5);
+            }
+            graphGeometry.colors[i] = color;
+        }
+        // faces are indexed using characters
+        const faceIndices = ['a', 'b', 'c', 'd'];
+        // copy the colors as necessary to the face's vertexColors array.
+        for (let i = 0; i < graphGeometry.faces.length; i++) {
+            const face = graphGeometry.faces[i];
+            const numberOfSides = (face instanceof Face3) ? 3 : 4;
+            for (let j = 0; j < numberOfSides; j++) {
+                const vertexIndex = face[faceIndices[j]];
+                face.vertexColors[j] = graphGeometry.colors[vertexIndex];
+            }
+        }
+
+        const wireMaterial = this.createWireMaterial(segments);
+        wireMaterial.map.repeat.set(segments, segments);
+
+        const graphMesh = new Mesh(graphGeometry, wireMaterial);
+        this.scene.add(graphMesh);
+    }
+
+    createWireMaterial(segments: number = 40): MeshBasicMaterial {
+        var loader = new TextureLoader();
+        const squareImageUrl = require('../images/square.png');
+        const wireTexture = loader.load(squareImageUrl);
+        wireTexture.wrapS = wireTexture.wrapT = RepeatWrapping;
+        wireTexture.repeat.set(segments, segments);
+        return new MeshBasicMaterial({ map: wireTexture, vertexColors: VertexColors, side: DoubleSide });
     }
 
 }
