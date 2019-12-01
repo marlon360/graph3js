aece22a63638bfbb652cedb4b31fa28d4c089f25
Marlon Lückert <m.lueckert@me.com>
2019-12-01 21:40:02 +0100

    input for curve

--- main.ts
+++ main.ts
@@ -4,12 +4,19 @@ import { Parser } from 'expr-eval';
 const graphViewer = new GraphViewer(document.body);
 
 function drawGraph() {
-    const funcInputElement = document.getElementById("func-input") as HTMLInputElement;
+    const funcInputElement = document.getElementById("graph-input") as HTMLInputElement;
     const funcText = funcInputElement.value;
     const zFunc = Parser.parse(funcText).toJSFunction("x, y");
     graphViewer.createGraph((x, y) => zFunc(x, y));
 }
+function drawCurve() {
+    const funcInputElement = document.getElementById("curve-input") as HTMLInputElement;
+    const funcText = funcInputElement.value;
+    const func = <(t: number) => Array<number>><unknown>Parser.parse(funcText).toJSFunction("t");
+    graphViewer.createCurve((t) => func(t));
+}
 
-document.getElementById("func-button").onclick = drawGraph;
+document.getElementById("graph-button").onclick = drawGraph;
+document.getElementById("curve-button").onclick = drawCurve;
 
 drawGraph();
