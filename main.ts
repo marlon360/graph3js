a8bd73fd5ba3aee9e37bad815bbd92ec358ef939
Marlon Lückert <m.lueckert@me.com>
2019-12-02 16:32:28 +0100

    read get parameter, parse input/output size

--- main.ts
+++ main.ts
@@ -4,20 +4,40 @@ import { MathParser } from './src/MathParser';
 
 const graphViewer = new GraphViewer(document.body);
 
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+
+function setInputValue(input: string) {
+    const funcInputElement = document.getElementById("graph-input") as HTMLInputElement;
+    funcInputElement.value = input;
+}
+
 function drawGraph() {
     const funcInputElement = document.getElementById("graph-input") as HTMLInputElement;
     const funcText = funcInputElement.value;
-    const zFunc = MathParser.parse(funcText);
-    graphViewer.createGraph((x, y) => zFunc(x, y));
-}
-function drawCurve() {
-    const funcInputElement = document.getElementById("curve-input") as HTMLInputElement;
-    const funcText = funcInputElement.value;
-    const func = <(t: number) => Array<number>><unknown>MathParser.parse(funcText);
-    graphViewer.createCurve((t) => func(t));
+    const parseResult = MathParser.parse(funcText);
+    if (parseResult.inputSize == 2 && parseResult.outputSize == 1) {
+        graphViewer.createGraph((x, y) => parseResult.func(x, y));
+    } else if (parseResult.inputSize == 1) {
+        graphViewer.createCurve((t) => parseResult.func(t));
+    }
 }
 
 document.getElementById("graph-button").onclick = drawGraph;
-document.getElementById("curve-button").onclick = drawCurve;
+
+const funcParameter = findGetParameter("func");
+if (funcParameter != null) {
+    setInputValue(decodeURIComponent(funcParameter));
+}
 
 drawGraph();
--- src/MathParser.ts
+++ src/MathParser.ts
@@ -2,9 +2,25 @@ import { Parser } from 'expr-eval';
 
 export class MathParser {
 
-    public static parse(input: string): (...args: number[]) => any {
+    public static parse(input: string): {
+        func: (...args: number[]) => any,
+        inputSize: number,
+        outputSize: number
+    }{
         input = this.stripFunctionDeclaration(input);
-        return Parser.parse(input).toJSFunction(this.getVariables(input).join(","));
+        const func = Parser.parse(input).toJSFunction(this.getVariables(input).join(","));
+        const inputSize = Parser.parse(input).variables().length;
+        const parameterArray = new Array(inputSize).fill(0);
+        
+        const tempResult = Parser.parse(input).toJSFunction(this.getVariables(input).join(","))(...parameterArray);
+        let outputSize = 1;
+        if (Array.isArray(tempResult)) {
+            outputSize = tempResult.length;
+        }
+
+        return {
+            func, inputSize, outputSize
+        };
     }
 
     static getVariables(input: string) {
